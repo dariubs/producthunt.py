4c3123e8762dff0391fc8f49bc655bc70414c093
Dariush Abbasi <dariushem@yahoo.com>
2023-10-25 09:02:56 +0330

    version 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name='producthunt',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
         'requests>=2.0.0,<3.0.0',
